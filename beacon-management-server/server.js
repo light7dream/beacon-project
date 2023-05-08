c8cd8ca876d0fff8820839aaa246e9534c4d5e89
marcosdpeon <130071026+marcosdpeon@users.noreply.github.com>
2023-05-07 19:27:12 -0700

    made server environment

--- beacon-management-server/server.js
+++ beacon-management-server/server.js
@@ -5,3 +5,29 @@ const path = require('path');
 const app = express();
 
 require('dotenv').config();
+
+app.use(cors());
+// parse requests of content-type - application/json
+app.use(express.json()); 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); 
+
+// set static folder - shared folder
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// routes
+require('./app/routes')(app);
+// database connection
+require('./app/config/db.config')()
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the beacon-management-system!" });
+});
+  
